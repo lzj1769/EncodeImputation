10ad4d7f5707f9d763496b088769d665b1f65abd
lzj1769 <lzj1769@gmail.com>
2019-05-08 12:59:24 +0200

    add

--- src/avocado_pytorch.zsh
+++ src/avocado_pytorch.zsh
@@ -7,23 +7,11 @@ export PATH=/usr/local_host/bin:$PATH
 export PATH=/home/rs619065/local/bin:$PATH
 export PATH=/home/rs619065/.local/bin:$PATH
 export PATH=/home/rs619065/local/bamtools/bin:$PATH
-export PATH=/home/rs619065/cuda/8.0.44/bin:$PATH
 export PATH=/home/rs619065/perl5/bin:$PATH
-export PATH=/home/rs619065/meme/bin:$PATH
-export PATH=/home/rs619065/AMUSED:$PATH
-export PATH=/home/rs619065/opt/IGVTools:$PATH
 
 ################################################################
 # LIBRARYPATH
 export LD_LIBRARY_PATH=/home/rs619065/local/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/rs619065/local/bamtools/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/rs619065/cuda/8.0.44/lib64:$LD_LIBRARY_PATH
-
-export R_LIBS_USER=$R_LIBS_USER:/home/rs619065/local/lib64/R/library
-export PERL5LIB=/home/rs619065/perl5/lib/5.26.1:$PERL5LIB
-export PERL5LIB=/home/rs619065/perl5/lib/perl5:$PERL5LIB
-
-export RUBYLIB=$RUBYLIB:/home/rs619065/AMUSED:/home/rs619065/Ruby-DNA-Tools
 
 source ~/miniconda2/bin/activate py36env
 
