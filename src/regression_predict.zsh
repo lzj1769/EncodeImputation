70047ede1f3ef7ec7968d4f1a58de66b01f9b407
lzj1769 <lzj1769@gmail.com>
2019-07-01 11:38:17 +0200

    add scripts

---
+++ src/regression_predict.zsh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+################################################################
+# PATH
+export PATH=/usr/bin:$PATH
+export PATH=/usr/local_host/bin:$PATH
+export PATH=/home/rs619065/local/bin:$PATH
+export PATH=/home/rs619065/.local/bin:$PATH
+export PATH=/home/rs619065/local/bamtools/bin:$PATH
+export PATH=/home/rs619065/perl5/bin:$PATH
+
+################################################################
+# LIBRARYPATH
+export LD_LIBRARY_PATH=/home/rs619065/local/lib:$LD_LIBRARY_PATH
+
+source ~/miniconda2/bin/activate py36env
+
+
+python regression_predict.py -c $1 -a $2 -o $3
